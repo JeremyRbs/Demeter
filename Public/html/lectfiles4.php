b9c4d45f62974edea8ae107342c8ac80ceaad366
Sean-HubGit <79507994+Sean-HubGit@users.noreply.github.com>
2021-06-08 11:00:47 +0200

    Update lectfiles4.php

--- Public/html/lectfiles4.php
+++ Public/html/lectfiles4.php
@@ -39,8 +39,11 @@ $return_arr = array();
 
 $query = "SELECT HeureDispo, NomClient,  GROUP_CONCAT( NomProd) as NomProd, commande.NumCom  from Detail JOIN com_det ON detail.Num_OF = com_det.Num_OF JOIN commande ON commande.NumCom = com_det.NumCom WHERE commande.A_Livrer = 'O' AND commande.EtatLivraison = 'N' GROUP BY NomClient";
 
+/*$query = "SELECT HeureDispo, NomClient, NomProd FROM COMMANDE JOIN COM_DET ON COM_DET.NumCom = COMMANDE.NumCom JOIN DETAIL ON COM_DET.Num_OF = DETAIL.Num_OF WHERE COMMANDE.A_Livrer = 'O'AND  COMMANDE.EtatLivraison = 'N'";*/
+
 $result = mysqli_query($con, $query);
 $i = 0;
+
 while ($row = mysqli_fetch_array($result)) {
 
     $heure = $row['HeureDispo'];
